4bac483a2ec266fbb951596397854f19b78fff4e
Sarath-33 <withsarath.m@gmail.com>
2025-11-05 12:04:05 +0530

    add the user to db in inngest.js

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -18,6 +18,8 @@ const syncUser = inngest.createFunction(
             name: `${first_name || ""} ${last_name || ""}`,
             profileImage: image_url,
         };
+
+         await User.create(newUser);
     }
 );
 
